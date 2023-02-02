3d4e5698a7a552855f93eba9cdcab6bcef69b62e
CandiceBrasileiro <candicelopes88@gmail.com>
2023-02-02 11:15:56 -0300

    "delete e edição"

--- index.js
+++ index.js
@@ -36,9 +36,75 @@ app.get("/games", (req, res) => {
 
 app.get("/game/:id", (req, res) => {
   if(isNaN(req.params.id)){
-    res.send("ISSO NÃO É UM NÚMERO")
+    res.sendStatus(400);
+    // res.send("ISSO NÃO É UM NÚMERO")
   }else{
-    res.send("ISSO É UM NÚMERO")
+    // res.send("ISSO É UM NÚMERO")
+
+    var id = parseInt(req.params.id);
+
+    var game = DB.games.find(g => g.id == id);
+
+    if(game != undefined){
+      res.statusCode = 200;
+      res.json(game);
+    }else{
+      res.sendStatus(404);
+    }
+  }
+})
+
+app.post("/game", (req, res) => {
+  var {title, year, price} = req.body;
+
+  DB.games.push({
+    id: 2323,
+    title,
+    price,
+    year
+  })
+
+  res.sendStatus(200);
+}) 
+
+app.delete("/game/:id", (req, res) => {
+  if(isNaN(req.params.id)){
+    res.sendStatus(400);
+  }else{
+    var id = parseInt(req.params.id);
+    var game = DB.games.findIndex(g => g.id == id);
+
+    if(index == -1){
+      res.sendStatus(404)
+    }else{
+      DB.games.splice(index, 1);
+      res.sendStatus(200);
+    }
+  }
+})
+
+app.put("/game/:id", (req, res)=> {
+  if(isNaN(req.params.id)){
+    res.sendStatus(400);
+  }else{
+    var id = parseInt(req.params.id);
+    var game = DB.games.findIndex(g => g.id == id);
+  }
+
+  if(game != undefined){
+    var {title, price, year} = req.body;
+
+    if(title != undefined){
+      game.title = title;
+    }
+    if(price != undefined){
+      game.price = price;
+    }
+    if(year != undefined){
+      game.year = year;
+    }
+  }else{
+    res.sendStatus(404);
   }
 })
 
