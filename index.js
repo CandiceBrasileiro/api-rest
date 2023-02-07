19b788958eb0637aad8715052c3bdb7d1d6b254b
CandiceBrasileiro <candicelopes88@gmail.com>
2023-02-07 11:27:37 -0300

    Finalização de autenticação com JWT

--- index.js
+++ index.js
@@ -13,8 +13,27 @@ app.use(bodyParser.json());
 
 function auth(req, res, next){
   const authToken = req.headers['authorization'];
-  console.log(authToken)
-  next();
+
+  if(authToken != undefined){
+
+    const bearer = authToken.split(' ');
+    var token = bearer[1];
+
+    jwt.verify(token, JWTSecret, (err, data) => {
+      if(err){
+        res.status(401);
+        res.json({err: "Token inválido"})
+      }else{
+        
+        req.token = token;
+        req.loggedUser = {id: data.id, email: data.email};
+        next();
+      }
+    });
+  }else{
+    res.status(401);
+    res.json({err: "Token inválido"});
+  }
 }
 
 var DB = {
