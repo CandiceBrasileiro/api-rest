e2718db4a4141b411d987bcf5be9c3c9419eab9d
CandiceBrasileiro <candicelopes88@gmail.com>
2023-02-07 10:45:13 -0300

    geração de token + autenticação

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const JWTSecret = "hdusadhaiusd";
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+function auth(req, res, next){
+  const authToken = req.headers['authorization'];
+  console.log(authToken)
+  next();
+}
 
 var DB = {
   games: [
@@ -29,10 +38,24 @@ var DB = {
       year: 2012,
       price: 20
     },
+  ],
+  users: [
+    {
+      id: 1,
+      name: "Candice",
+      email: "candicelopes88@gmail.com",
+      password: "java123"
+    },
+    {
+      id: 2,
+      name: "Raquel",
+      email: "raquel.bras@gmail.com",
+      password: "123node"
+    }
   ]
 }
 
-app.get("/games", (req, res) => {
+app.get("/games", auth, (req, res) => {
   res.statusCode = 200;
   res.json(DB.games);
 })
@@ -110,6 +133,39 @@ app.put("/game/:id", (req, res)=> {
   }
 })
 
+app.post("/auth", (req, res) => {
+
+  var {email, password} = req.body;
+
+  if(email != undefined){
+
+    var user = DB.users.find(u => u.email == email);
+    if(user != undefined){
+
+      if(user.password == password){
+
+        jwt.sign({id: user.id, email: user.email}, JWTSecret,{expiresIn: '48h'}, (err, token) => {
+          if(err){
+            res.status(400);
+            res.json({err:"Falha interna"});
+          }else{
+            res.status(200);
+            res.json({token: token});
+          }
+        })
+      } else {
+        res.status(401);
+        res.json({err: "Credenciais inválidas!"})
+      }
+    }else{
+      res.status(400);
+      res.json({err: "O e-mail enviado não existe na base de dados!"});
+    }
+  }else{
+    res.status(400);
+    res.json({err: "O e-mail enviado é inválido"})
+  }
+});
 
 app.listen(45678,() => {
   console.log("API RODANDO!")
